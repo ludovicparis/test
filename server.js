2f3100c02a8a6fa32e20225251838d90338dec25
Ludovic Verdon <ludovic.verdon@oxygenepme.com>
2017-06-09 14:23:07 +0200

    2eme essai

--- server.js
+++ server.js
@@ -47,6 +47,8 @@ app.get('/update', function (req, res) {
   res.render("clientList", {clients : clientList});
 });
 
-app.listen(80, function () {
+
+var port = (process.env.PORT || 80)
+app.listen(port, function () {
   console.log("Server listening on port 80");
 });
